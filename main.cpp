ff286b533b3644eef4944e6f614d2c42733a7c5e
Dongun614 <72856107+Dongun614@users.noreply.github.com>
2025-03-13 15:13:36 +0900

    update: main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,38 @@
 //일단 기본적은 메뉴 출력까지만 해보자
 //메뉴는 I,D,R,N,P,Q
+//Quit을 하기 전에는 코드가 계속해서 돌아가야 한다
+//while문을 사용하는데 음
 
 #include <iostream>
+#include <cctype>
 using namespace std;
 
 void showMenu(void);
 
 int main (void){
-    char choice;
-    showMenu();
+    string choice;
+    bool running = true;
 
-    //여기에 스위치 문
-    cout << "Choose menu: ";
-    cin >> choice;
-    cout << choice << endl;
+    while(running){
+        showMenu();
+        cout << "Choose menu: ";
+        cin >> choice;
+
+        if(choice == "I" || choice == "i"){
+            cout << "I 입니다" << endl;
+        }else if(choice == "D" || choice == "d"){
+            cout << "D 입니다" << endl;
+        }else if(choice == "R" || choice == "r"){
+            cout << "R 입니다" << endl;
+        }else if(choice == "N" || choice == "n"){
+            cout << "N 입니다" << endl;
+        }else if(choice == "P" || choice == "p"){
+            cout << "P 입니다" << endl;
+        }else if(choice == "Q" || choice == "q"){
+            cout << "프로그램을 종료합니다." << endl;
+            running = false;
+        }else cout << "잘못된 입력입니다.\n" << endl;
+    }
 
     return 0;
 }
@@ -25,5 +44,5 @@ void showMenu(void){
     cout << "R : Retrieve the element with the largest kev." << endl;
     cout << "N : Increase the kev of an element in the queue." << endl;
     cout << "P : Print all elements in the queue." << endl;
-    cout << "Q : Quit." << endl;
+    cout << "Q : Quit.\n" << endl;
 }
