f5461ad24703387fd25b1b6a5ac9de4b4d9123f0
Dongun614 <72856107+Dongun614@users.noreply.github.com>
2025-03-14 15:42:59 +0900

    main: 구조잡기

--- main.cpp
+++ main.cpp
@@ -1,15 +1,62 @@
 //일단 기본적은 메뉴 출력까지만 해보자
 //메뉴는 I,D,R,N,P,Q
 //Quit을 하기 전에는 코드가 계속해서 돌아가야 한다
-//while문을 사용하는데 음
+//insert를 어떻게 짜면 될까
+//들어오면 element로 받아서
+//h -> heap[index]에 넣기
 
 #include <iostream>
 #include <cctype>
 using namespace std;
 
+typedef struct{
+    string name;
+    int score;
+    string className;
+} element;
+
+class Heap{
+    private:
+    element* arr;
+    int size;
+    int capacity;
+
+    void resize(){
+        capacity *= 2;
+        element* newArr = new element[capacity];
+        for(int i=1; i<size; i++){
+            newArr[i] = arr[i];
+        }
+        delete[] arr;
+        arr = newArr;
+    }
+
+    public:
+        Heap(){
+            size = 1;
+            capacity = 1;
+            arr = new element[capacity];
+        }
+
+        ~Heap() {
+            delete[] arr;
+        }
+
+        void insert(element e){
+            if(size == capacity){
+                resize();
+            }
+            arr[size++] = e;
+        }
+};
+
 void showMenu(void);
+void heapInit(Heap* h);
+void insert(Heap* h, element e);
 
 int main (void){
+    Heap h;
+
     string choice;
     bool running = true;
 
