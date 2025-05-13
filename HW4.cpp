c21e2de944095efcdc51923a51d3de718910912f
Dongun614 <72856107+Dongun614@users.noreply.github.com>
2025-05-13 11:14:43 +0900

    Feat: dp 구성 완료

--- HW4.cpp
+++ HW4.cpp
@@ -1,7 +1,59 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Item {
+    int weight;
+    int value;
+};
 
 int main(){
-    printf("Hello world\n");
+    vector<Item> items = {
+        {2, 3},
+        {3, 4},
+        {4, 5},
+        {5, 6}
+    };
+
+    int s = items.size();
+    int w = 5;
     
+    cout << "s: " << s << "   w: " << w << endl;
+
+    int dp[w+1][s+1];
+
+    for(int i=0; i<=w; i++){
+        for(int j=0; j<=s; j++){
+            dp[i][j] = 0;
+        }
+    }
+
+    // 작성 과정
+    for(int j=1; j<=s; j++){ 
+        int current_w = items[j-1].weight;
+        int current_b = items[j-1].value;
+        
+        for(int i=0; i<=w; i++){ 
+            if(current_w <= i){ 
+                if(current_b + dp[i-current_w][j-1] > dp[i][j-1]){
+                    dp[i][j] = current_b + dp[i-current_w][j-1];
+                } else {
+                    dp[i][j] = dp[i][j-1];
+                }
+            } else { 
+                dp[i][j] = dp[i][j-1];
+            }
+        }
+    }
+
+    // 출력 과정
+    for(int i=0; i<=w; i++){
+        for(int j=0; j<=s; j++){
+            cout << dp[i][j] << "  ";
+            if(j==s) cout << endl;
+        }
+    }
+
     return 0;
 }
